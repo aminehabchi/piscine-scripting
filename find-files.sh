44e9239b1c77482dfe5ef39c0b85ccf4a22f05ff
aminehabchi <amine.habchi.01@gmail.com>
2026-01-19 13:55:28 +0100

    find files

--- find-files.sh
+++ find-files.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-curl -s -O https://assets.01-edu.org/devops-branch/find-files-example.zip
-
-unzip -q find-files-example.zip
-
-find find-files-example -type f -name "*z"
+find. -type f -name "*z"
 
 
 
-find find-files-example -name "a*" 
+find . -name "a*" 
