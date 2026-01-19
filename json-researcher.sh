f0fa1a6937389ee7ba8a817f1089b9461eafc35e
aminehabchi <amine.habchi.01@gmail.com>
2026-01-19 11:58:17 +0100

    done

--- json-researcher.sh
+++ json-researcher.sh
@@ -3,8 +3,7 @@
 
 curl -s https://learn.zone01oujda.ma/assets/superhero/all.json > file.json
 
-jq '.. | objects | select(.id? == 1) | .. | .power? // empty' file.json
-
+ jq '.. | objects | select(.id? == 1) | {name, power}' file.json
 
 
 
