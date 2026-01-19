2076e54f94676d46e51501ad26c8048f869a971e
aminehabchi <amine.habchi.01@gmail.com>
2026-01-19 12:27:23 +0100

    head and tail

---
+++ head-and-tail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -s "https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt" | head -n 1
+
+curl -s "https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt" | tail -n 1
+
