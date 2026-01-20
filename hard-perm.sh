d81419905b8843bb40693e5875dbadb223319fc0
aminehabchi <amine.habchi.01@gmail.com>
2026-01-20 10:07:18 +0100

    hard-perm

--- hard-perm.sh
+++ hard-perm.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-chmod 701 hard-perm/0   # dr-------x
-chmod 777 hard-perm/3   # drwxrwxrwx
-chmod 701 hard-perm/A   # dr-------x
+# Directories
+chmod 401 hard-perm/0
+chmod 777 hard-perm/3
+chmod 401 hard-perm/A
 
-chmod 604 hard-perm/1   # -r------w-
-chmod 604 hard-perm/9   # -r------w-
-chmod 604 hard-perm/5   # -r--rw----
-chmod 604 hard-perm/6   # -r--rw----
-chmod 604 hard-perm/2   # -rw----r--
-chmod 604 hard-perm/8   # -rw----r--
-chmod 505 hard-perm/4   # -r-x--x---
-chmod 505 hard-perm/7   # -r-x--x---
-
+# Files
+chmod 402 hard-perm/1
+chmod 604 hard-perm/2
+chmod 501 hard-perm/4
+chmod 460 hard-perm/5
+chmod 460 hard-perm/6
+chmod 510 hard-perm/7
+chmod 604 hard-perm/8
+chmod 402 hard-perm/9
