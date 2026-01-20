e56bc80898a336236df30e8e908e1b8da1a118e6
aminehabchi <amine.habchi.01@gmail.com>
2026-01-20 13:16:05 +0100

    file-detail

--- file-details.sh
+++ file-details.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls -l hard-perm --no-group --time-style="+%Y-%m-%d %H:%M" | sed '1d' | awk '{$2=""; $3=""; $4=""; print}' | sed 's/  \+/ /g'
+ls -l --no-group --time-style="+%Y-%m-%d %H:%M" | sed '1d' | awk '{$2=""; $3=""; $4=""; print}' | sed 's/  \+/ /g'
 
 
 
