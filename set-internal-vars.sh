94156177efbf6e23d688c09e5e9a51b681462cfb
aminehabchi <amine.habchi.01@gmail.com>
2026-01-21 15:47:26 +0100

    set-env-vars

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Create variables
 MY_MESSAGE="Hello World"
 MY_NUM=100
 MY_PI=3.142
 MY_ARR=("one" "two" "three" "four" "five")
 
-# Print variables
 echo "$MY_MESSAGE"
 echo "$MY_NUM"
 echo "$MY_PI"
-# Print array elements separated by comma and space
 echo "${MY_ARR[*]}" 
---
+++ set-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export MY_MESSAGE="Hello World"
+export MY_NUM=100
+export MY_PI=3.142
+
+echo "$MY_MESSAGE"
+echo "$MY_NUM"
+echo "$MY_PI"
