b0d85ffe1f409903d9601b5a0af7c688c9310d69
aminehabchi <amine.habchi.01@gmail.com>
2026-01-22 10:57:37 +0100

    auto-jobs

--- auto-jobs.sh
+++ auto-jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf '41 20 * * 5 /bin/echo "01"' > task1
+printf '41 20 * * 5 echo "01"' > task1
 printf '* * * * * /bin/sh /home/user01/check' > task2
 printf '0 0 * * * /bin/sh /home/user01/backup' > task3
 printf '@reboot 01exec' > task4
