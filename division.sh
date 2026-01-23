e0b6135121fef357f485e055b4aad9800b8967c6
aminehabchi <amine.habchi.01@gmail.com>
2026-01-23 12:07:45 +0100

    division

---
+++ division.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Error: two numbers must be provided"
+    exit 1
+fi
+
+num1="$1"
+num2="$2"
+
+
+integer_regex='^-?[0-9]+$'
+
+if ! [[ $num1 =~ $integer_regex && $num2 =~ $integer_regex ]]; then
+    echo "Error: both arguments must be integers"
+    exit 1
+fi
+
+
+if [ "$num2" -eq 0 ]; then
+    echo "Error: division by zero is not allowed"
+    exit 1
+fi
+
+
+result=$(( num1 / num2 ))
+
+echo "$result"
