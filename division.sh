0033422e966f4ca26d3db5e4505df1d13d0b99ad
aminehabchi <amine.habchi.01@gmail.com>
2026-01-23 12:28:33 +0100

    division

--- division.sh
+++ division.sh
@@ -5,23 +5,20 @@ if [ $#  -lt 2 ]; then
     exit 1
 fi
 
-num1="$1"
-num2="$2"
-
 
 integer_regex='^-?[0-9]+$'
 
-if ! [[ $num1 =~ $integer_regex && $num2 =~ $integer_regex ]]; then
+if ! [[ $1 =~ $integer_regex && $2 =~ $integer_regex ]]; then
     echo "Error: both arguments must be integers"
     exit 1
 fi
 
 
-if [ "$num2" -eq 0 ]; then
+if [ "$2" -eq 0 ]; then
     echo "Error: division by zero is not allowed"
     exit 1
 fi
 
-result=$(( num1 / num2 ))
 
-echo "$result"
+
+echo $(( $1 / $2 ))
