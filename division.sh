40e66558ea6f0bec19e89758bef6c363a9eb2dc3
aminehabchi <amine.habchi.01@gmail.com>
2026-01-23 12:41:10 +0100

    division

--- division.sh
+++ division.sh
@@ -6,13 +6,13 @@ if [ $#  -lt 2 ]; then
 fi
 
 
-integer_regex='^-?[0-9]+$'
 
-fi
-if [[ ! "$a" =~ ^[+-]?[0-9]+$ || ! "$b" =~ ^[+-]?[0-9]+$ ]]; then
+if [[ ! "$1" =~ ^[+-]?[0-9]+$ || ! "$2" =~ ^[+-]?[0-9]+$ ]]; then
     echo "Error: both arguments must be integers"
     exit 0
 fi
+
+
 
 if [ "$2" -eq 0 ]; then
     echo "Error: division by zero is not allowed"
