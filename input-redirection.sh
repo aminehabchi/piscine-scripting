98fff1bc25c8c8bcd33b304130e19b23cf118b05
aminehabchi <amine.habchi.01@gmail.com>
2026-01-23 11:20:28 +0100

    input-redirection

--- input-redirection.sh
+++ input-redirection.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-cat << 'EOF' > show-info.sh
-#!/bin/sh
-
-cat -e << INFO
-The current directory is: $(pwd)
+cat <<EOF > show-info.sh
+#!/bin/bash
+cat -e <<INFO
+The current directory is: $PWD
 The default paths are: $PATH
 The current user is: $USER
 INFO
