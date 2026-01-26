96cabfc64091b7f1a8df960dd99f4108a5c86db7
aminehabchi <amine.habchi.01@gmail.com>
2026-01-26 18:05:41 +0100

    clean_the_list

---
+++ clean_the_list.py
@@ -0,0 +1,12 @@
+def clean_list(list):
+    t=False
+    
+    for i, l in enumerate(list):
+        l=l.strip().capitalize()
+        if l=="Milk":
+            t=True
+        list[i]=str(i+1)+" /"+l
+    if t==False:
+        list.append(f"{len(list)+1}/ Milk")
+    return list
+
