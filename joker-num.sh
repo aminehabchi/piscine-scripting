fe124287c6b36c37c4eb53fd817271e6e53ac705
aminehabchi <amine.habchi.01@gmail.com>
2026-01-26 11:16:40 +0100

    jocker

---
+++ joker-num.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: wrong argument"
+    exit 1
+fi
+
+if [ $1 -lt 0 ]; then
+    echo "Error: wrong argument"
+    exit 1
+fi
+
+for i in {1..5}
+do
+    echo "Enter your guess ($((5 - i + 1)) tries left):"
+    read num
+
+    if [ -z "$num" ]; then
+        continue
+    elif ! [[ $num =~ ^[0-9]+$ ]]; then
+        continue
+    elif [ "$num" -eq "$1" ]; then
+        echo "Congratulations, you found the number in $i moves!"
+        exit 0
+    elif [ "$num" -lt "$1" ]; then
+        echo "Go up"
+    else
+        echo "Go down"
+    fi
+
+done
+
+
+echo "You lost, the number was $1"
