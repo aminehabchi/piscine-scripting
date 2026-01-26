d02a5f05368c489fa122eaaf31a4c364f15ae4c9
aminehabchi <amine.habchi.01@gmail.com>
2026-01-26 12:01:25 +0100

    grades

--- grades.sh
+++ grades.sh
@@ -10,20 +10,20 @@ if ! [[ "$1" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-notes=()
+notes=()  
 
 for (( i=1; i<=$1; i++ )); do
     read -p "Student Name #$i: " name
     read -p "Student Grade #$i: " grade
 
     if [ -z "$grade" ]; then
-        echo "Error: The grade '$grade' is not a valid number. Only numerical grades between 0 and 100 are accepted."
+        echo "Error: The grade '$grade' is not valid. Only numerical grades between 0 and 100 are accepted."
         exit 1
     elif ! [[ "$grade" =~ ^[0-9]+$ ]]; then
-        echo "Error: The grade '$grade' is not a valid number. Only numerical grades between 0 and 100 are accepted."
+        echo "Error: The grade '$grade' is not valid. Only numerical grades between 0 and 100 are accepted."
         exit 1
-    elif  [ "$grade" -gt 100 ]; then
-        echo "Error: The grade '$grade' is not a valid number. Only numerical grades between 0 and 100 are accepted."
+    elif [ "$grade" -gt 100 ]; then
+        echo "Error: The grade '$grade' is not valid. Only numerical grades between 0 and 100 are accepted."
         exit 1
     fi
 
@@ -36,9 +36,7 @@ for (( i=1; i<=$1; i++ )); do
     else
         notes+=("$name: You had a poor performance!")
     fi
-
 done
-
 
 for (( i=0; i<${#notes[@]}; i++ )); do
     echo "${notes[i]}"
