57ba9656f2cee8b690a20120d20790530a7f1d54
aminehabchi <amine.habchi.01@gmail.com>
2026-01-29 09:59:36 +0100

    flex_function.py

--- flex_function.py
+++ flex_function.py
@@ -1,16 +1,19 @@
 
 
 
-def create_person(first_name, last_name, age, gender, size=1.83, job="taxidermist"):
-    d = dict()
-    d["first_name"] = first_name
-    d["last_name"] = last_name
-    d["age"] = age
-    d["gender"] = gender
-    d["size"] = size
-    d["job"] = job
-    return d
+
+def create_person(first_name, last_name, age, gender, *, size=1.83, job="taxidermist"):
+
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "age": age,
+        "gender": gender,
+        "size": size,
+        "job": job
+    }
 
 
 person = create_person("Kevin", "Boulin", 34, "male")
 print(person)
+
