f2431f56d99a3fc4119d78094636097d542c2001
aminehabchi <amine.habchi.01@gmail.com>
2026-01-29 10:24:05 +0100

    greastest-of-all

--- greatest-of-all.sh
+++ greatest-of-all.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 
-c=0
+max=0  
 
-max=0 
-while true; do
-    c=$((c + 1))
+for i in {1..10}; do
     echo -n "Enter a number: "
     read -r line
+
     if [[ -z "$line" ]]; then
-        break
-    fi
-    if [[ ! $line =~ ^[0-9]+$ ]]; then
         echo "ERROR: Invalid input only positive numerical characters are allowed"
         exit 1
     fi
 
-    if [[ $line -lt 0 ]]; then
-        echo "ERROR: Invalid input, only positive numbers are allowed"
+    if ! [[ "$line" =~ ^[0-9]+$ ]]; then
+        echo "ERROR: Invalid input only positive numerical characters are allowed"
         exit 1
     fi
 
-    if [[ $line -gt 10000 ]]; then
+    if [[ "$line" -gt 10000 ]]; then
         echo "ERROR: The number entered is too large"
         exit 1
     fi
-    
-    if [[ $line -gt $max ]]; then
+
+    if [[ "$line" -gt "$max" ]]; then
         max=$line
     fi
-
-    if [[ $c == 10 ]]; then
-        break
-    fi
 done
-
-
 
 echo "The largest number is: $max"
