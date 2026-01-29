95fa62a9888c72864a270d9ace3175c8ce09926e
aminehabchi <amine.habchi.01@gmail.com>
2026-01-29 10:36:09 +0100

    comparator.sh

--- comparator.sh
+++ comparator.sh
@@ -2,16 +2,16 @@
 
 if [[ $# -ne 2 ]]; then
     echo "Error: The script only works with two arguments!"
-    exit 1
+    exit 0
 fi
 
 arg1=$1
 arg2=$2
 
 
-if ! [[ "$arg1" =~ ?[0-9]+$ ]] || ! [[ "$arg2" =~ ?[0-9]+$ ]]; then
+if ! [[ "$arg1" =~ ^-?[0-9]+$ ]] || ! [[ "$arg2" =~ ^-?[0-9]+$ ]]; then
     echo "Error: Only two numeric arguments are acceptable!"
-    exit 1
+    exit 0
 fi
 
 if [[ "$arg1" -gt "$arg2" ]]; then
