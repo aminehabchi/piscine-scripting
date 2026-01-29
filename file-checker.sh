22f4613b45132b404548ac55c52ab2f69af6d35b
aminehabchi <amine.habchi.01@gmail.com>
2026-01-29 10:39:11 +0100

    file-checker

---
+++ file-checker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo "Error: No file provided"
+    exit 1
+fi
+
+
+if [[ -e "$1" ]]; then
+    echo "File exists"
+
+    if [[ -r "$1" ]]; then
+        echo "File is readable"
+    else
+        echo "File is not readable"
+    fi
+
+    if [[ -w "$1" ]]; then
+        echo "File is writable"
+    else
+        echo "File is not writable"
+    fi
+
+    if [[ -x "$1" ]]; then
+        echo "File is executable"
+    else
+        echo "File is not executable"
+    fi
+else
+    echo "File does not exist"
+fi
