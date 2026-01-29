98f42db1f95cbe3352759a728cbe86c5e3c727f2
aminehabchi <amine.habchi.01@gmail.com>
2026-01-29 10:03:25 +0100

    flex_function.py

--- flex_function.py
+++ flex_function.py
@@ -17,3 +17,5 @@ def create_person(first_name, last_name,age, gender, *,  size=1.83, job="taxider
 person = create_person("Kevin", "Boulin", age=34, gender="male")
 print(person)
 
+a = create_person("Jane", "Doe", size=1.75, job="programmer")
+print(a)
