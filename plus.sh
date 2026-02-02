f2cfcc1327f66f1cebac3f53b56000d6a9c3d2f3
aminehabchi <amine.habchi.01@gmail.com>
2026-02-02 10:30:08 +0100

    plus

--- plus.sh
+++ plus.sh
@@ -1,4 +1,24 @@
+#!/bin/sh
 
+# Check if both arguments exist
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Error: You must provide two numbers."
+    exit 1
+fi
 
+# Check if $1 is a number
+echo "$1" | grep -Eq '^-?[0-9]+$'
+if [ $? -ne 0 ]; then
+    echo "Error: First argument is not a valid number."
+    exit 1
+fi
 
-expr $1 + $2 
+# Check if $2 is a number
+echo "$2" | grep -Eq '^-?[0-9]+$'
+if [ $? -ne 0 ]; then
+    echo "Error: Second argument is not a valid number."
+    exit 1
+fi
+
+# If everything is valid, do the calculation
+expr "$1" + "$2"
